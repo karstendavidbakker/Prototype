e48ac52c0415ffe96dd856f8694c4bdfc5f9798a
Karsten Bakker <KARSTENDB94@GMAIL.COM>
2019-09-23 16:17:56 +0200

    new name property

--- Bluetooth_Gatt_example.py
+++ Bluetooth_Gatt_example.py
@@ -40,9 +40,8 @@ def handle_orientation_data(handle, value_bytes):
 #
     print("Received data: %s (handle %d)" % (str(value_bytes), handle))
     values = [float(x) for x in value_bytes.decode('utf-8').split(",")]
-    find_or_create("Left Wheel Orientation",
+    find_or_create("Left Wheel Orientation 2",
                    PropertyType.FOUR_DIMENSIONS).update_values(values)
-
 
 def discover_characteristic(device):
     """List characteristics of a device"""
