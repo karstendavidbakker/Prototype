b134336b1a8fb2371e70df3cf6879ca393138d2c
Cazikaza <casperbeeris@hotmail.com>
2019-09-27 16:34:41 +0200

    println->print

--- Serial_to_terminal.py
+++ Serial_to_terminal.py
@@ -23,9 +23,9 @@ def serial_to_property_values():
         line = line_bytes.decode('utf-8')
         # Split the string using commas as separatoqr, we get a list of strings
         values = line.split(',')
-        println(line_bytes)
-        println(line)
-        println(values)
+        print(line_bytes)
+        print(line)
+        print(values)
 
 
 while True:
