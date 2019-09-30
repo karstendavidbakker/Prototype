06ea44137f29713e104927c1d74a72358e6f5a5f
Cazikaza <casperbeeris@hotmail.com>
2019-09-30 16:42:01 +0200

    simpelest solution?

--- Serial_to_terminal.py
+++ Serial_to_terminal.py
@@ -15,16 +15,17 @@ except :
 # Read the next line from the serial port
 # and update the property values
 def serial_to_property_values():
+    print(ser.readline())
     # Read one line
-    line_bytes = ser.readline()
+#    line_bytes = ser.readline()
     # If the line is not empty
-    if len(line_bytes) > 0:
+#    if len(line_bytes) > 0:
         # Convert the bytes into string
-        line = line_bytes.decode('utf-8')
+#        line = line_bytes.decode('utf-8')
         # Split the string using commas as separatoqr, we get a list of strings
-        values = line.split(',')
+#        values = line.split(',')
 #        print(line_bytes)
-        print(line)
+#        print(line)
 #        print(values)
 
 
