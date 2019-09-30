f20a09af033c5dfd5b99ee33e4e93892bdff9c5a
Karsten Bakker <KARSTENDB94@GMAIL.COM>
2019-09-30 11:16:38 +0200

    added API to read, list and create sensors

--- web/server.py
+++ web/server.py
@@ -8,6 +8,19 @@ def hello_world():
 if __name__ == '__main__':
     app.run()
 
+@app.route('/api/sensors', methods = ['GET'])
+def list():
+    return str(sensors)
+
+@app.route('/api/sensors/<path:sensor_id>', methods = ['GET'])
+def read(sensor_id):
+    global sensors
+    return sensors[sensor_id]
+
+@app.route('/api/sensors', methods = ['POST'])
+def create():
+    sensors.append(request.json["sensorName"])
+    return 'Added sensor!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
