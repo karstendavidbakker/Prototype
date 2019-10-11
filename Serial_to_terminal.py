cec41e361eae67c101658e144679d752fe18619d
Cazikaza <casperbeeris@hotmail.com>
2019-10-11 13:53:02 +0200

    : to , in serial to terminal

--- Serial_to_terminal.py
+++ Serial_to_terminal.py
@@ -23,10 +23,10 @@ def serial_to_property_values():
         # Convert the bytes into string
         line = line_bytes.decode('utf-8')
         # Split the string using commas as separatoqr, we get a list of strings
-#        values = line.split(',')
+       values = line.replace(':',',').split(',')
 #        print(line_bytes)
         print(line)
-#        print(values)
+       print(values)
 
 
 while True:
