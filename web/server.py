88ef010a0255b41ebb00d2cb92c23f0734208393
Karsten Bakker <KARSTENDB94@GMAIL.COM>
2019-10-14 13:47:44 +0200

    merge savetofile.py and server.py

--- web/server.py
+++ web/server.py
@@ -7,9 +7,14 @@ import time
 from random import random
 
 file = open("my-property.csv", "a") #we append to barchart
+current_time = int(datetime.datetime.utcnow().timestamp()*1000)
+values = (str(current_time), str(random()), str(random()), str(random()))
 
 # name site(created by flask) app
 app = Flask(__name__)
+
+app.config['SECRET KEY'] = 'secret!'
+socketio = SocketIO(app)
 # define amount of sensors
 sensors = ['sensor1', 'sensor2', 'sensor3']
 # functions for the root folder
@@ -65,7 +70,16 @@ def gauge():
 #added to test
 @app.route('/barchart')
 def barchart():
-    return render_template('barchart2.html')
+    xvalue = value1
+    return render_template('barchart2.html', xvalue = xvalue)
+
+@socketio.on('x-axis')
+def handle_x(json):
+    print(float(json['x-axis']))
+
+
+#xvalue = xvalue to link the variable to the web
+
     #return render_template('barchart.html') Name of variable)
 
 def write_in_csv(values):
@@ -73,7 +87,7 @@ def write_in_csv(values):
     file.write('\n')
 
 def write_to_json(values):
-    fileJSON = open("web/static/data.json", "w") # we write
+    fileJSON = open("static/data.json", "w") # we write
     fileJSON.write('[{"title":"x","subtitle":"TARGET motion in x direction","ranges":[20,25,30],"measures":[' + values[1] + ',23],"markers":[26]},{"title":"y","subtitle":"ORIENTATION motion in y direction","ranges":[350,500,600],"measures":['+ values[2] +',320],"markers":[550]},{"title":"z","subtitle":"BUMB motion in z direction","ranges":[5000,20000,25000],"measures":['+values[3]+',1650],"markers":[2100]}]')
     fileJSON.close();
     #print(values[1])
@@ -90,31 +104,40 @@ def write_to_json(values):
     print(value2)
     print(value3)
 
-try:
-    # finally, we call our function to start generating dum values
-    while True:
-        current_time = int(datetime.datetime.utcnow().timestamp()*1000)
-        print(current_time)
-        values = (str(current_time), str(random()), str(random()), str(random()))
-        write_in_csv(values)
+    try:
+        socketio.emit('x-axis','{"axisX": "%s"}' % str(value1), broadcast=True)
+    except:
+        print("No thing")
 
-        write_to_json(values)
-        #have a 2-second break
-        time.sleep(2)
-
-
-except Exception:
-        file.close()
-        fileJSON.close();
-
-        # have a 2-second break
-        time.sleep(2)
-
-except Exception:
-    file.close()
+        #turning it into a json object to any client that is accesing your page.
+# def randomValues():
+#     try:
+#         # finally, we call our function to start generating dum values
+#         while True:
+#             current_time = int(datetime.datetime.utcnow().timestamp()*1000)
+#             print(current_time)
+#             values = (str(current_time), str(random()), str(random()), str(random()))
+#             write_in_csv(values)
+#
+#             write_to_json(values)
+#             #have a 2-second break
+#             # time.sleep(2)
+#
+#
+#     except Exception:
+#             file.close()
+#             fileJSON.close();
+#
+#             # have a 2-second break
+#             time.sleep(2)
+#
+#     except Exception:
+#         file.close()
+write_in_csv(values)
+write_to_json(values)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    socketio.run(app, host = '0.0.0.0')
     # to give everyone from every pc access to web
 
 ###############################################################################################
