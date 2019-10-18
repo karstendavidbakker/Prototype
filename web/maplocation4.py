7879c90ca50c1db45aff52a7592f98602a61f0b4
Karsten Bakker <KARSTENDB94@GMAIL.COM>
2019-10-18 16:46:30 +0200

    .

--- web/maplocation4.py
+++ web/maplocation4.py
@@ -14,19 +14,22 @@ from flask_socketio import SocketIO, emit, send
 #print(distVal) EMITTING TO SOCKET CHANEL
 
 #get information from serial
+@socketio.on('gpslocation')
+def handle_gpslocation(json):
+    print( float(json['gpslocation']))
 
 
 
-    ser = Serial(port, 115200, timeout = 1)
 
+#ser = Serial(port, 115200, timeout = 1)
+
+    #if it comes from the phone need to pull it from the dcd server
     #
-    #
-    #
-    #
-
+    lat = [52.001737,52,51,56];
+    lng = [4.372825,5,53,5];
 
     try:
-       socketio.emit('marker', '{"lat": "lng"}' % str(position), broadcast=True) #emitting it to the websocket, broadcast is that everyone gets the same information
+       socketio.emit('marker', '{"lat": "%s", "lng": "%s"}' % str(lat[0]), broadcast=True) #emitting it to the websocket, broadcast is that everyone gets the same information
     except:
        print("No socket?") #tells there is a problem
     return distVal #...
